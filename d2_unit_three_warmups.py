bdeb673b72bf5c491bf5739c58b3e94c5fb3c61b
Musa <musa@iMac10.local>
2018-09-26 14:04:02 -0400

    information returned from a function

---
+++ d2_unit_three_warmups.py
@@ -0,0 +1,12 @@
+def add_one(number):
+    number += 1
+    print("number in function is", number)  # number is 6
+
+
+def main():
+    number = 5
+    add_one(number)
+    print("number in main is,", number)  # number is 5
+
+
+main()
---
+++ d3_unit_three_warmups.py
@@ -0,0 +1,10 @@
+def area_of_circle(radius):
+    area = 3.14 * radius ** 2
+    return area
+
+
+def main():
+    height = 9
+    radius = 12
+    Volume_of_cylinder = area_of_circle(radius) * height
+    print(Volume_of_cylinder)
