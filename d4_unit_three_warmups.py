7c9bbb9a3489281c93f70b73d39c67b3e8a19c53
Musa <musa@iMac10.local>
2018-09-28 08:21:09 -0400

    writing document strings

---
+++ d4_unit_three_warmups.py
@@ -0,0 +1,9 @@
+def area_of_rectangle(length, width):
+    """
+    finding area of rectangle by multiplying length by width
+    :param length: 
+    :param width:
+    :return:
+    """
+    area = length * width
+    return area
