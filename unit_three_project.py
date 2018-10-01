6e9516dca926f25e605fa709eb99e3cf61eeadce
Musa <musa@iMac10.local>
2018-10-01 10:11:57 -0400

    Final Draft of Unit Three Project

--- unit_three_project.py
+++ unit_three_project.py
@@ -1,8 +1,10 @@
 # Name : Musa Saleem
-# Date : 9 / 28
-# Last Modified : 9 / 28
-# Comments : On this project I made five crosses that connect with one another
-
+# Date : 10 / 1
+# Last Modified : 10 / 1
+# Comments : On this project I made five crosses that connect with one another.
+# I got the users input on what they wanted. Based on their desires of side length, and color.
+# This turtle program draws a middle cross with four crosses attached to each of the primary positions:
+# East, West, North and South.
 
 
 import turtle
@@ -10,13 +12,17 @@ turtle.speed(12)
 
 
 def side_length():
+    """
+    This function gets each consecutive side length that the user desires
+    :return:
+    """
     length = input("What is the length you want")
     return float(length)
 
 
 def fill_color():
     """
-    This function gets the inside color of the cross
+    This function gets the inside color of the cross that the user desires
     :return:
     """
     color = input("what do you want the inside color of the cross to be?")
@@ -24,6 +30,12 @@ def fill_color():
 
 
 def cross(length, color):
+    """
+    Is the process for drawing a single cross
+    :param length:
+    :param color:
+    :return:
+    """
     turtle.color(color)
     turtle.begin_fill()
 
@@ -52,7 +64,14 @@ def cross(length, color):
     turtle.forward(length)
     turtle.end_fill()
 
+
 def move(x, y):
+    """
+    Moves turtle to new location without drawing a line
+    :param x:
+    :param y:
+    :return:
+    """
     turtle.penup()
     turtle.goto(x, y)
     turtle.pendown()
